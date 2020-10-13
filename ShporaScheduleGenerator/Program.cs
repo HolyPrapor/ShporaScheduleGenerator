3ec4759b0d8803b6fe38c53678598ad808eda626
Цуп Илья Олегович <tsup@skbkontur.ru>
2020-10-13 12:15:26 +0500

    Edited output format to match Google Tables

--- ShporaScheduleGenerator/Program.cs
+++ ShporaScheduleGenerator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 
@@ -46,15 +47,39 @@ namespace ShporaScheduleGenerator
                 line = Console.ReadLine();
             }
 
-            foreach (var mentor in ScheduleGenerator.GenerateSchedule(firstStudentGroup, secondStudentGroup,
-                mentorGroups))
+            var studentList = new Dictionary<string, string[]>();
+
+            using (var mentorFile = File.CreateText("C://mentors.txt"))
             {
-                Console.WriteLine(mentor.Name);
-                for (var i = 0; i < mentor.ReviewedStudents.Count - 1; i++)
-                    Console.WriteLine($"\t {i + 1}. {string.Join(", ", mentor.ReviewedStudents[i])}");
+                foreach (var mentor in ScheduleGenerator.GenerateSchedule(firstStudentGroup, secondStudentGroup,
+                    mentorGroups))
+                {
+                    var mentorLine = mentor.Name + "\t";
+                
+                    for (var i = 0; i < 6; i++)
+                        for (var j = 0; j < 2; j++)
+                        {
+                            if (j < mentor.ReviewedStudents[i].Count)
+                                mentorLine += $"{mentor.ReviewedStudents[i][j]}\t";
+                            else
+                                mentorLine += "\t";
+                            if(j < mentor.ReviewedStudents[i].Count && !studentList.ContainsKey(mentor.ReviewedStudents[i][j]))
+                                studentList[mentor.ReviewedStudents[i][j]] = new string[6];
+                            if(j < mentor.ReviewedStudents[i].Count)
+                                studentList[mentor.ReviewedStudents[i][j]][i] = mentor.Name;
+                        }
+                    mentorFile.WriteLine(mentorLine);
+                }
             }
 
-            Console.ReadKey();
+            using (var studentFile = File.CreateText("C://students.txt"))
+            {
+                foreach (var studentPair in studentList)
+                {
+                    studentFile.Write($"{studentPair.Key}\t");
+                    studentFile.WriteLine(string.Join("\t", studentPair.Value));
+                }
+            }
         }
     }
 }
